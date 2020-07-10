f41f37fbe753ec378e4c0c95ef7e26c0f86cd08f
Guilherme Silva <gmsilva83@outlook.com>
2020-07-09 21:50:27 -0400

    add component infrastructure/main-menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MainMenuComponent } from "./infrastructure/main-menu/main-menu.component";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'teste-capitani-front';
+  title = 'teste Guilherem Silva';
+
+  constructor() {
+
+  }
 }
